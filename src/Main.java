7e14d74869b6205dd4e61bdc923b6fd8492e2444
Дмитрий <dimvik007@gmail.com>
2026-07-06 14:41:42 +1000

    домашка перед

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
-void main() {
-    System.out.println("Hello and welcome!");
-    //Задача 1;
-    System.out.println("Задача 1");
-    String firstName = "Ivan";
-    String middleName = "Ivanovich";
-    String lastName = "Ivanov";
-    String fullName = lastName + " " + firstName + " " + middleName;
-    System.out.println("Ф.И.О. сотрудника — " + fullName);
-    //Задача 2;
-    System.out.println("Задача 2");
-    System.out.println(" Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
-    //Task 3
-    System.out.println("Задача 3");
-    fullName = "Иванов Семён Семёнович";
-    String fullNameWithoutYo = fullName.replace('ё', 'e');
-    System.out.println("Данные ФИО сотрудника — " + fullNameWithoutYo);
+public class Main {
+
+
 }
+
+
